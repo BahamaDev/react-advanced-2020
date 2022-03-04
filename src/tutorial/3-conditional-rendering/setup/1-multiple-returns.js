cc51c208e3d8cd91936d1e1386208a39dc81f9ed
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2022-03-04 07:23:52 -0500

    Multiple conditions

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,46 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
+  const [isloading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(user);
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isloading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h1>{user}</h1>
+      </div>
+    );
+  }
+
   return <h2>multiple returns</h2>;
 };
 
